aefb741c66554d42abee5f54e005c3a8510c7f54
FVGhost <146720629+FVGhost@users.noreply.github.com>
2023-10-30 18:17:17 +0000

    Add files via upload

--- book.sql
+++ book.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 17, 2023 at 04:34 AM
+-- Generation Time: Oct 30, 2023 at 07:17 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -37,6 +37,18 @@ CREATE TABLE `book` (
   `TotalCopies` int(11) NOT NULL,
   `Description` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`Bookid`, `Title`, `AuthorID`, `ISBN`, `PublicationYear`, `GenreID`, `TotalCopies`, `Description`) VALUES
+(1, 'Harry Potter and the Philosopher\'s Stone\r\n', 1, 2147483647, 1997, 1, 1200000, 'An 11-year-old orphan living with his unwelcoming aunt, uncle, and cousin, who learns of his own fame as a wizard known to have survived his parents\' murder at the hands of the dark wizard Lord Voldemort as an infant when he is accepted to Hogwarts School'),
+(2, 'Harry Potter and the Chamber of Secrets', 1, 123589026, 1998, 2, 770000, 'Throughout the summer holidays after his first year at Hogwarts School of Witchcraft and Wizardry, Harry Potter has been receiving sinister warnings from a house-elf called Dobby. Now, back at school to start his second year, Harry hears unintelligible wh'),
+(3, 'Harry Potter and the Prisoner of Azkaban', 1, 830827838, 1999, 3, 650000, 'Harry Potter & The Prisoner of Azkaban is about Harry\'s 3rd year at Hogwarts. Along with friends Ron and Hermione, Harry investigates the case of Sirius Black, an escaped prisoner from Azkaban, the wizard prison.'),
+(4, 'Harry Potter and the Goblet of Fire', 1, 387086705, 2000, 4, 40000, 'It follows Harry Potter, a wizard in his fourth year at Hogwarts School of Witchcraft and Wizardry, and the mystery surrounding the entry of Harry\'s name into the Triwizard Tournament, in which he is forced to compete. The book was published in the United'),
+(5, 'Harry Potter and the Order of the Phoenix', 1, 691461178, 2003, 5, 660000, 'Dark times have come to Hogwarts. After the Dementors\' attack on his cousin Dudley, Harry Potter knows that Voldemort will stop at nothing to find him. There are many who deny the Dark Lord\'s return, but Harry is not alone: a secret order gathers at Grimm'),
+(6, 'Charlie and the Chocolate Factory', 6, 927491739, 1964, 6, 2600000, 'Charlie And The Chocolate Factory is a delightful children\'s book by Roald Dahl. It tells the story of a young boy named Charlie Bucket, who wins a golden ticket and gets the opportunity to tour the mysterious and magical chocolate factory owned by the ec');
 
 --
 -- Indexes for dumped tables
