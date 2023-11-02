92844e3fc11e35e359aa5d288d13bf8109f6725e
Rob <124254988+Robbi333@users.noreply.github.com>
2023-11-02 17:03:07 +0000

    Update Main.java

--- LibraryApp/src/main/java/LibraryUtils/Main.java
+++ LibraryApp/src/main/java/LibraryUtils/Main.java
@@ -3,6 +3,6 @@ package LibraryUtils;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Swag");
-        System.out.println("testing");
+        System.out.println("");
     }
 }
