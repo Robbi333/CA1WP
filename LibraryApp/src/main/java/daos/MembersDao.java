fa1d4dc202a6c98dd56387c939db61384737d997
Rob <124254988+Robbi333@users.noreply.github.com>
2023-11-02 20:02:39 +0000

    Update MembersDao.java

--- LibraryApp/src/main/java/daos/MembersDao.java
+++ LibraryApp/src/main/java/daos/MembersDao.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class MembersDao {
+public class MembersDao extends Dao {
 
     private final String libraryapp;
 
