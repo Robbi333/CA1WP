8cff53e9073550dcaf9f8e602d31dd2abf559789
Destiny <146720629+FVGhost@users.noreply.github.com>
2023-11-02 18:13:35 +0000

    Create genre.java

---
+++ LibraryApp/src/main/java/Business/genre.java
@@ -0,0 +1,50 @@
+package Business;
+
+public class genre {
+
+    private int genreid;
+    private String genre1;
+    private String genre2;
+
+    public int getGenreid() {
+        return genreid;
+    }
+
+    public void setGenreid(int genreid) {
+        this.genreid = genreid;
+    }
+
+    public String getGenre1() {
+        return genre1;
+    }
+
+    public void setGenre1(String genre1) {
+        this.genre1 = genre1;
+    }
+
+    public String getGenre2() {
+        return genre2;
+    }
+
+    public void setGenre2(String genre2) {
+        this.genre2 = genre2;
+    }
+
+    @Override
+    public String toString() {
+        return "genre{" +
+                "genreid=" + genreid +
+                ", genre1='" + genre1 + '\'' +
+                ", genre2='" + genre2 + '\'' +
+                '}';
+    }
+
+    public genre(int genreid, String genre1, String genre2) {
+        this.genreid = genreid;
+        this.genre1 = genre1;
+        this.genre2 = genre2;
+
+
+
+    }
+}
