cfb14c12efdfba56dd445d144769b3195ef0b110
Destiny <146720629+FVGhost@users.noreply.github.com>
2023-11-02 18:07:11 +0000

    author class

---
+++ LibraryApp/src/main/java/Business/author.java
@@ -0,0 +1,59 @@
+package Business;
+
+public class author {
+
+    private int authorid;
+    private String firstname;
+    private String lastname;
+    private int bookid;
+
+    public author(int authorid, String firstname, String lastname, int bookid) {
+        this.authorid = authorid;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.bookid = bookid;
+    }
+
+    public int getAuthorid() {
+        return authorid;
+    }
+
+    public void setAuthorid(int authorid) {
+        this.authorid = authorid;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getBookid() {
+        return bookid;
+    }
+
+    public void setBookid(int bookid) {
+        this.bookid = bookid;
+    }
+
+    @Override
+    public String toString() {
+        return "author{" +
+                "authorid=" + authorid +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", bookid=" + bookid +
+                '}';
+    }
+
+}
