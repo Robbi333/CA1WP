48f0f82e4960ac9c7742f956f303ed3c07fb86a3
Rob <124254988+Robbi333@users.noreply.github.com>
2023-11-03 20:13:19 +0000

    changes

--- LibraryApp/src/test/java/daos/MemberDaoTest.java
+++ LibraryApp/src/test/java/daos/MemberDaoTest.java
@@ -1,57 +1,56 @@
 import Business.members;
 import daos.MembersDao;
 import exceptions.DaoException;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Date;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
+@ExtendWith(MockitoExtension.class)
 public class MembersDaoTest {
 
-    private TestMembersDao testMembersDao;
+    @Mock
+    private Connection mockConnection;
 
-    @BeforeAll
-    public static void setupDatabase() throws SQLException {
-        // Set up your in-memory database (e.g., H2) and create necessary tables
-        Connection connection = DriverManager.getConnection("jdbc:h2:mem:testLibraryApp;DB_CLOSE_DELAY=-1");
-        Statement statement = connection.createStatement();
-        statement.execute("CREATE TABLE members ("
-                + "MemberID INT AUTO_INCREMENT PRIMARY KEY,"
-                + "Username VARCHAR(255),"
-                + "Password VARCHAR(255),"
-                + "First_Name VARCHAR(255),"
-                + "Last_Name VARCHAR(255),"
-                + "Email VARCHAR(255),"
-                + "Address1 VARCHAR(255),"
-                + "Address2 VARCHAR(255),"
-                + "Eircode VARCHAR(255),"
-                + "Phone_Number VARCHAR(255),"
-                + "Registration_Date DATE,"
-                + "Admin BOOLEAN"
-                + ")");
-        connection.close();
-    }
+    @Mock
+    private PreparedStatement mockPreparedStatement;
 
     @BeforeEach
-    public void setUp() {
-        // Initialize your test DAO using the test database configuration
-        testMembersDao = new TestMembersDao();
+    void setUp() throws SQLException {
+        when(mockConnection.prepareStatement(anyString())).thenReturn(mockPreparedStatement);
     }
 
     @Test
-    public void testInsertMember() throws DaoException {
-        // Create a test member
+    public void testInsertMember() throws SQLException, DaoException {
         members member = new members("testUser", "testPassword", "John", "Doe", "john@example.com",
                 "123 Main St", "Apt 4B", "12345", "555-555-5555", Date.valueOf("2023-11-02"));
 
-        // Insert the member
-        boolean result = testMembersDao.insertMember(testMembersDao.getConnection(), member);
+        when(mockPreparedStatement.executeUpdate()).thenReturn(1);
 
-        // Assert that the insertion was successful
+        boolean result = MembersDao.insertMember(mockConnection, member);
+
+        verify(mockConnection).prepareStatement(anyString());
+        verify(mockPreparedStatement).setString(1, "testUser");
+        verify(mockPreparedStatement).setString(2, "testPassword");
+        verify(mockPreparedStatement).setString(3, "John");
+        verify(mockPreparedStatement).setString(4, "Doe");
+        verify(mockPreparedStatement).setString(5, "john@example.com");
+        verify(mockPreparedStatement).setString(6, "123 Main St");
+        verify(mockPreparedStatement).setString(7, "Apt 4B");
+        verify(mockPreparedStatement).setString(8, "12345");
+        verify(mockPreparedStatement).setString(9, "555-555-5555");
+        verify(mockPreparedStatement).setDate(10, Date.valueOf("2023-11-02"));
+        verify(mockPreparedStatement).setBoolean(11, false); // Assuming default is not an admin
+
         assertEquals(true, result);
     }
 }
