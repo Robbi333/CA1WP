35fc6755d656c5955aef12882e4f498aba968d2b
Destiny <146720629+FVGhost@users.noreply.github.com>
2023-11-03 03:53:27 +0000

    bookinterface

--- LibraryApp/src/main/java/daos/BookDaoInterface.java
+++ LibraryApp/src/main/java/daos/BookDaoInterface.java
@@ -6,6 +6,16 @@ import java.util.List;
 
 public interface BookDaoInterface {
 
+
+
+    boolean borrrowBook(int MemberID, int bookid) throws DaoException;
+
+    boolean returnBook(int MemberID, int bookid) throws DaoExcecption;
+
+    boolean canBorrowBooks(int MemberID, int bookid) throws DaoException;
+
     public List<book> findAllBooks() throws DaoException;
+
+
 
 }
