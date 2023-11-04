c17a71f537cdd9376e0afe572f7e4559e0ac779d
Rob <124254988+Robbi333@users.noreply.github.com>
2023-11-04 14:56:51 +0000

    fixed members dao test

---
+++ LibraryApp/src/test/java/daos/MembersDaoTest.java
@@ -0,0 +1,37 @@
+package daos;
+
+import Business.members;
+import exceptions.DaoException;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+ class MembersDaoTest {
+
+    @Test
+    public void testInsertMember() throws SQLException, DaoException {
+        members testMember = new members("testUser", "testPassword", "John", "Doe", "john@example.com",
+                "123 Main St", "Apt 4B", "12345", "555-555-5555", Date.valueOf("2023-11-02").toLocalDate());
+
+        Connection dbConn = mock(Connection.class);
+        PreparedStatement ps = mock(PreparedStatement.class);
+        ResultSet rs = mock(ResultSet.class);
+
+
+        when(dbConn.prepareStatement(anyString())).thenReturn(ps);
+        when(ps.executeUpdate()).thenReturn(1);
+
+        MembersDao membersDao = new MembersDao("testLibraryApp");
+        boolean result = membersDao.insertMember(dbConn, testMember);
+
+        assertEquals(true, result);
+    }
+    }
+}
