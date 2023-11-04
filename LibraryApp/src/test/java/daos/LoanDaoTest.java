55351588275d3ef6aaeaa4c6971ab4dc4198939b
Leo <leozelion31@gmail.com>
2023-11-04 14:52:08 +0000

    added test names for loan class

---
+++ LibraryApp/src/test/java/daos/LoanDaoTest.java
@@ -0,0 +1,40 @@
+package daos;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoanDaoTest {
+
+    @Test
+    void viewActiveLoans() {
+    }
+
+    @Test
+    void getLoansForMember() {
+    }
+
+    @Test
+    void borrowBook() {
+    }
+
+    @Test
+    void canBorrowBook() {
+    }
+
+    @Test
+    void returnbook() {
+    }
+
+    @Test
+    void isLate() {
+    }
+
+    @Test
+    void isCreditCardValid() {
+    }
+
+    @Test
+    void payLateFeeValidate() {
+    }
+}
